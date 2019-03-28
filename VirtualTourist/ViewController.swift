13bf1611b9881b560737196e1afdbd2f6936eb60
Justin <justinkampen@hotmail.com>
2019-03-27 19:16:53 -0700

    Initial Commit

---
+++ VirtualTourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VirtualTourist
+//
+//  Created by Justin Kampen on 3/27/19.
+//  Copyright © 2019 Justin Kampen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
